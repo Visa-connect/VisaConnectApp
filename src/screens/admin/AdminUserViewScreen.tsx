a9832506aed046d6c550920d3503c2e2c836876b
Arron Linton <lintonarron@gmail.com>
2025-10-14 11:48:25 -0400

    removed unused var

--- src/screens/admin/AdminUserViewScreen.tsx
+++ src/screens/admin/AdminUserViewScreen.tsx
@@ -9,7 +9,6 @@ import {
   MapPinIcon,
   BriefcaseIcon,
   CalendarIcon,
-  ShieldCheckIcon,
 } from '@heroicons/react/24/outline';
 import Button from '../../components/Button';
 import { adminUserService, AdminUser } from '../../api/adminUserService';
